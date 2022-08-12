9f5788093c564e24b85203c3aa84f97e523c1048
Hillary Muller <hillaryaward@gmail.com>
2022-08-11 19:01:18 -0500

    commit tree e483d1353e4d8fe71d1e57173755caf3c7900e0f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.length < 3
+        return array.join(' and ')
+    elsif array.length >= 3
+        last = array.pop
+        and_last = ", and #{last}"
+        return array.join(", ") + and_last
+    end
 end
